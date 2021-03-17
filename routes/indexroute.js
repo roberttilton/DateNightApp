90694fcf665bd53092b691a18d4994016b152b47
Robert Tilton <robtilton2086@gmail.com>
2021-03-17 13:53:44 -0400

    edited some html and routes

--- routes/indexroute.js
+++ routes/indexroute.js
@@ -2,18 +2,19 @@ const router = require('express').Router();
 const path = require('path');
 
 router.get('/index', (req, res) => {
-    response.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-router.get('/secondpage', (request, response) => {
-    response.sendFile(path.join(__dirname, '../public/secondpage.html'));
+router.get('/secondpage', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/secondpage.html'));
 });
 
-router.get('/generatedcard', (request, response) => {
-    response.sendFile(path.join(__dirname, '../public/generatedcard.html'));
+router.get('/generatedcard', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/generatedcard.html'));
 });
 
 router.get('*', (req, res) => {
-    response.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
+
 module.exports = router;
