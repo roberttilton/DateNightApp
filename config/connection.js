09d6821faf80527a184d45b54e3d3922b43297d5
Robert Tilton <robtilton2086@gmail.com>
2021-03-18 15:09:07 -0400

    post joe session, lots of things working including routes

--- config/connection.js
+++ config/connection.js
@@ -2,16 +2,13 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  'dates_db',
-  'root',
-  'password', {
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD, 
+  {
     host: 'localhost',
     dialect: 'mysql',
-<<<<<<< HEAD
-    port: 3001,
-=======
-    port: process.env.PORT || 3001,
->>>>>>> 11cc80e37f9c6a33cc61fbf734e0c21e92ef576e
+    port: 3306,
   }
 );
 
--- routes/api/index.js
+++ routes/api/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const userRoutes = require('./user-routes.js');
 
 // URL: /api/user
+console.log('about to invoke user routes');
 router.use('/user', userRoutes);
 
 module.exports = router;
