121299109051659fa9edef6a8c763b173635c9fb
Isiah <feeleyisiah@gmail.com>
2021-03-18 15:11:55 -0400

    updated js and html

--- config/connection.js
+++ config/connection.js
@@ -4,12 +4,7 @@ require('dotenv').config();
 const sequelize = new Sequelize(
   'dates_db',
   'root',
-<<<<<<< HEAD
-  'password',
-  {
-=======
   'password', {
->>>>>>> 11cc80e37f9c6a33cc61fbf734e0c21e92ef576e
     host: 'localhost',
     dialect: 'mysql',
     port: process.env.PORT || 3001,
