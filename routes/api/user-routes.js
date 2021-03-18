f4a78ddfc2ef7895c599e72b119cc35b8cfa7666
Navid Ebahimi <navidmebrahimi22@gmail.com>
2021-03-18 10:15:00 -0400

    saving to push to main

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,7 +1,10 @@
+const path = require('path');
+const fs = require('fs')
 const router = require('express').Router();
 const {
     User
 } = require('../../models');
+
 
 router.post('/', async (req, res) => {
     try {
