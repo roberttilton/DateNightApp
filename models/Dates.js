336204a9da29906c1b5e49d2fffdc3e523eb5b5c
Navid Ebahimi <navidmebrahimi22@gmail.com>
2021-03-18 12:29:22 -0400

    saving to pull from main

--- models/Dates.js
+++ models/Dates.js
@@ -1,49 +1,48 @@
 const {
-    Model,
-    DataTypes
-  } = require('sequelize');
-  const sequelize = require('../config/config');
-  
-  // create our User model
-  class Dates extends Model {}
-  
-  Dates.init({
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    activity: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    activity_title: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    address: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    date: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'user',
-          key: 'id',
-        },
-    }
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'Dates'
-  });
+  Model,
+  DataTypes
+} = require('sequelize');
+const sequelize = require('../config/config');
 
-  module.exports = Dates;
+// create our User model
+class Dates extends Model {}
+
+Dates.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  activity: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  business_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  address: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  date: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'user',
+      key: 'id',
+    },
+  }
+}, {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'Dates'
+});
+
+module.exports = Dates;
