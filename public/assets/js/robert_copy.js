d0c951fc3063697779902261ab8c7ee50656b226
Robert Tilton <robtilton2086@gmail.com>
2021-03-20 16:37:10 -0400

    got api call fucking working

--- public/assets/js/robert_copy.js
+++ public/assets/js/robert_copy.js
@@ -1,18 +1,24 @@
-var yelpContent = document.getElementById('exampleFormControlSelect2');
-// example code below
-var userLocation = document.querySelector('#activity-field').value;
-var typeInput = document.querySelector('#location-field').value;
+// var yelpContent = document.getElementById('exampleFormControlSelect2');
+// // example code below
+// var userLocation = document.querySelector('#activity-field').value;
+// var typeInput = document.querySelector('#location-field').value;
 
 function renderYelp() {
-	const URL = `https://api.yelp.com/v3/businesses/search?term=${typeInput}&location=${userLocation}`;
+	console.log('renderYelp entered...');
+	var yelpContent = document.getElementById('exampleFormControlSelect2');
+	var userLocation = document.querySelector('#location-field').value;
+	var typeInput = document.querySelector('#activity-field').value;
+	const URL = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${typeInput}&location=${userLocation}`;
 	console.log(URL);
 	axios.get(URL, {
 		headers: {
-			'Authorization': 'rCAnqlM_1ZnGvXkWuH1wspttqOUMehZuLYGdZFD4s2XZx3mMOLTTuUFaXL0rpUpNXDWuwy42jCTJ78dMkXeKxAtZmGJ2Yzr1bfiXgB-ORDBMou_WVtBQCG6U679LYHYx'
+			Authorization: 'Bearer rCAnqlM_1ZnGvXkWuH1wspttqOUMehZuLYGdZFD4s2XZx3mMOLTTuUFaXL0rpUpNXDWuwy42jCTJ78dMkXeKxAtZmGJ2Yzr1bfiXgB-ORDBMou_WVtBQCG6U679LYHYx',
+			'Content-Type': 'application/json'
 		}
 	})
 	.then(function (response) {
-		return response.json();
+		console.log(response);
+		return response.json;
 	})
 	.then(
 		// function that parses the returned array and applies it where necessary
@@ -39,11 +45,19 @@ function renderYelp() {
 	.catch(error => console.error(error));
 }
 
+// const formEl = document.querySelector('#datePosts');
+// console.log("formEl", formEl);
+// formEl.addEventListener('submit', function(event) {
+// 	event.preventDefault();
+// 	console.log('submit event');
+// 	renderYelp();
+// })
+
 document.querySelector("#populate")
 		.addEventListener("click", function (event) {
 		event.preventDefault();
 		console.log('submit event');
-		console.log(typeInput);
-		console.log(userLocation);
+		// console.log(typeInput);
+		// console.log(userLocation);
 		renderYelp();
 	});
